d2cdfd03b1b98e7460948f85d5da3a22f35e800d
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-23 19:58:28 +0700

    improve update contact

--- script.js
+++ script.js
@@ -1,86 +1,79 @@
-const contacts = [
+let allContacts = [
   {
     id: 1,
     name: "Muhammad Rizki",
     email: "muhammadrizki05@gmail.com",
     phone: "+6212345678",
-    isALive: true,
+    isAlive: true,
+    city: "Palembang",
   },
-
   {
     id: 2,
     name: "Monica",
     email: "monica@example.com",
     phone: "+6212345678",
-    isALive: true,
+    isAlive: true,
+    city: "Jakarta",
   },
-
   {
     id: 3,
     name: "Shafa",
     email: "shafana@example.co.id",
     phone: "+6253536737",
     isAlive: false,
+    city: "Bandung",
   },
 ];
-
-// for (let index = 0; index < contacts.length; index++) {
-//   const contact = contacts[index];
-//   console.log(`
-//     id : ${contact.id}
-//     name :${contact.name}
-//     email : ${contact.email}
-//     phone :${contact.phone}
-//     isAlive : ${contact.isAlive}
-
-//     `);
-// }
 
 // READ
 function displayContacts() {
   console.log("\n=== Contact List ===");
-  contacts.forEach((contact) => {
+  allContacts.forEach((contact) => {
     console.log(`
     id: ${contact.id}
     name: ${contact.name}
     email: ${contact.email}
     phone: ${contact.phone}
-    address: ${contact.address || "-"}
-    isAlive: ${contact.isAlive}
+    city: ${contact.city || "-"}
+    alive: ${contact.isAlive}
     `);
   });
 }
 
 // CREATE
 function createContact(newContact) {
-  const nextId = contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 1;
+  const nextId =
+    allContacts.length > 0 ? allContacts[allContacts.length - 1].id + 1 : 1;
   const contactToAdd = {
     ...newContact,
     id: nextId,
   };
-  contacts.push(contactToAdd);
+  allContacts.push(contactToAdd);
   console.log("Contact successfully created.");
   return contactToAdd;
 }
 
 // UPDATE
-function updateContact(id, updateData) {
-  const contactIndex = contacts.findIndex((contact) => contact.id === id);
-  if (contactIndex !== -1) {
-    contacts[contactIndex] = {
-      ...contacts[contactIndex],
-      ...updateData,
-    };
-    console.log(`Contact with id ${id} updated successfully.`);
-  } else {
-    console.log(`Contact with id ${id} not found.`);
-  }
+function updateContact(id, newContactData) {
+  const updatedAllContacts = allContacts.map((oneContact) => {
+    if (oneContact.id === id) {
+      return {
+        ...oneContact, // id, name, email, phone, city, ....
+        ...newContactData, // name, city, ...
+      };
+    } else {
+      return oneContact;
+    }
+  });
+
+  allContacts = updatedAllContacts;
+  console.log(`Contact id:${id} has been updated`);
 }
 
 function deleteContact(id) {
-  const contactIndex = contacts.findIndex((contact) => contact.id === id);
+  const contactIndex = allContacts.findIndex((contact) => contact.id === id);
   if (contactIndex !== -1) {
-    contacts.splice(contactIndex, 1);
+    allContacts.splice(contactIndex, 1);
     console.log(`Contact with id ${id} deleted successfully.`);
   } else {
     console.log(`Contact with id ${id} not found.`);
@@ -92,38 +85,39 @@ function deleteContact(id) {
 // -----------------
 
 // CREATE EXAMPLES
-createContact({
-  name: "Budi",
-  email: "budi@example.com",
-  phone: "+6288888888",
-  address: "Jakarta",
-  isAlive: true,
-});
+// createContact({
+//   name: "Budi",
+//   email: "budi@example.com",
+//   phone: "+6288888888",
+//   address: "Jakarta",
+//   isAlive: true,
+// });
 
-createContact({
-  name: "Mochammad",
-  email: "mochammad@example.net",
-  phone: "+629121445255",
-  address: "Jogja",
-  isAlive: false,
-});
+// createContact({
+//   name: "Mochammad",
+//   email: "mochammad@example.net",
+//   phone: "+629121445255",
+//   address: "Jogja",
+//   isAlive: false,
+// });
 
-createContact({
-  name: "Michael Phelps",
-  email: "michael.phelps@example.net",
-  phone: "+622564674",
-  address: "Baltimore, Maryland, USA",
-  isAlive: true,
-});
+// createContact({
+//   name: "Michael Phelps",
+//   email: "michael.phelps@example.net",
+//   phone: "+622564674",
+//   address: "Baltimore, Maryland, USA",
+//   isAlive: true,
+// });
 
 // READ
-displayContacts();
+// displayContacts();
 
 // UPDATE
-updateContact(2, { name: "Monica Bellucci", address: "Italy" });
+updateContact(2, { name: "Monica Bellucci", city: "Rome" });
+displayContacts();
 
 // DELETE
-deleteContact(3);
+// deleteContact(3);
 
 // READ AGAIN TO SEE CHANGES
-displayContacts();
+// displayContacts();
